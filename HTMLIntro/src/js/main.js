55748fea640d3ab9a6d6077559c3ccbc33207808
Gabrielle Ashley <a.gabby1219@gmail.com>
2019-02-06 16:53:50 -0600

    added main.js messing with React

---
+++ HTMLIntro/src/js/main.js
@@ -0,0 +1,18 @@
+var root = document.getElementById('root');
+var content = React.createElement('p', {}, "Hi there"); //paragraph is element, {} holds the attributes elements, then children
+
+
+// var content = (
+//     <div>
+//         <h1>Hello World</h1>
+//         <a href="mailto:dude@example.com">
+//         <h1>React in Action</h1>
+
+//         </a>
+//     </div>
+// )
+
+ReactDOM.render
+(
+    content, root
+)
